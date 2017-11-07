a897871b454f2a187bdb7c2ad3f093d8ef3a8907
Rakib Hasan <rakib_hasan6@yahoo.com>
2017-11-07 20:00:22 +0600

    Added odd occurrence in array

---
+++ src/OddOccurrencesInArray.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class OddOccurrencesInArray {
+
+    public static void main(String[] args) {
+        int A[] = {7, 9,3,9,3,9,9};
+        System.out.println("" + solution(A));
+    }
+
+    static int solution(int[] A) {
+        HashSet<Integer> set = new HashSet<Integer>(A.length/2);
+
+        for(int i = 0; i < A.length; i++) {
+            if (set.contains(A[i])) {
+                set.remove(A[i]);
+            } else {
+                set.add(A[i]);
+            }
+        }
+        return (int) set.toArray(new Integer[0])[0];
+    }
+}
