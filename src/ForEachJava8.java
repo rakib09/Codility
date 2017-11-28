e1e9fe345b37b936d676016f2bdd0682a68620b6
Rakib Hasan <rakib_hasan6@yahoo.com>
2017-11-29 00:56:24 +0600

    Added forech

---
+++ src/ForEachJava8.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ForEachJava8 {
+
+    public static void main() {
+        Map<String, Integer> items = new HashMap<>();
+        items.put("A", 10);
+        items.put("B", 20);
+        items.put("C", 30);
+        items.put("D", 40);
+        items.put("E", 50);
+        items.put("F", 60);
+
+        items.forEach((k,v)->System.out.println("Item : " + k + " Count : " + v));
+
+        items.forEach((k,v)->{
+            System.out.println("Item : " + k + " Count : " + v);
+            if("E".equals(k)){
+                System.out.println("Hello E");
+            }
+        });
+    }
+}
